92c8414acf8bb3cddb6b7780ca8adedeb9c8c43a
burnsy2830 <liamburns@cmail.carleton.ca>
2025-01-28 12:41:43 -0500

    Bug fix: Script sends configs and reads from the right ports now

--- main/projectStarShotWrapper.py
+++ main/projectStarShotWrapper.py
@@ -2,9 +2,12 @@ import struct
 import time
 import numpy as np
 import serial
+import codecs  
+import binascii 
+import math  
 
 # -------------------Configuration-----------------------------------
-CFG_FILE = "area_scanner_68xx_ODS.cfg"
+CFG_FILE = "test.cfg"
 COM_PORTS = {"cfg": "COM6", "data": "COM4"}
 BAUDRATE = 921600
 #---------------------End of Configuration---------------------------
@@ -12,6 +15,8 @@ BAUDRATE = 921600
 
 #-----------------Magic Word---------------------------
 MAGIC_WORD = [2, 1, 4, 3, 6, 5, 8, 7]
+TC_PASS   =  0
+TC_FAIL   =  1
 #----------------End of magic word---------------------
 
 
@@ -43,14 +48,182 @@ def send_config(port, commands):
             print(f"Sent: {cmd}")
 
 
+
+
+
 #---------------------End of parsing logic-------------------
 
+
+#----------------Start of Packet Parsing---------------------
+def checkMagicPattern(data):
+    """!
+       This function check if data arrary contains the magic pattern which is the start of one mmw demo output packet.  
+
+        @param data : 1-demension byte array
+        @return     : 1 if magic pattern is found
+                      0 if magic pattern is not found 
+    """ 
+    found = 0
+    if (data[0] == 2 and data[1] == 1 and data[2] == 4 and data[3] == 3 and data[4] == 6 and data[5] == 5 and data[6] == 8 and data[7] == 7):
+        found = 1
+    return (found)
+
+
+def parse_gtrack_targets(payload):
+    """Parses GTRACK target data from the payload."""
+    target_size = 40  # Each target data block is 40 bytes
+    num_targets = len(payload) // target_size
+    targets = []
+    for i in range(num_targets):
+        offset = i * target_size
+        tid = struct.unpack('<I', payload[offset:offset + 4])[0]
+        posX, posY, posZ = struct.unpack('<fff', payload[offset + 4:offset + 16])
+        velX, velY, velZ = struct.unpack('<fff', payload[offset + 16:offset + 28])
+        accX, accY, accZ = struct.unpack('<fff', payload[offset + 28:offset + 40])
+        targets.append({
+            'tid': tid,
+            'posX': posX, 'posY': posY, 'posZ': posZ,
+            'velX': velX, 'velY': velY, 'velZ': velZ,
+            'accX': accX, 'accY': accY, 'accZ': accZ
+        })
+    return targets
+def parser_helper(data, readNumBytes,debug=False):
+    """!
+       This function is called by parser_one_mmw_demo_output_packet() function or application to read the input buffer, find the magic number, header location, the length of frame, the number of detected object and the number of TLV contained in this mmw demo output packet.
+
+        @param data                   : 1-demension byte array holds the the data read from mmw demo output. It ignorant of the fact that data is coming from UART directly or file read.  
+        @param readNumBytes           : the number of bytes contained in this input byte array  
+            
+        @return headerStartIndex      : the mmw demo output packet header start location
+        @return totalPacketNumBytes   : the mmw demo output packet lenght           
+        @return numDetObj             : the number of detected objects contained in this mmw demo output packet          
+        @return numTlv                : the number of TLV contained in this mmw demo output packet           
+        @return subFrameNumber        : the sbuframe index (0,1,2 or 3) of the frame contained in this mmw demo output packet
+    """ 
+    
+
+    headerStartIndex = -1
+
+    for index in range (readNumBytes):
+        #if data[index:index+8:1].__len__() == 8:
+            if checkMagicPattern(data[index:index+8:1]) == 1:
+                headerStartIndex = index
+                break
+    
+    if headerStartIndex == -1: # does not find the magic number i.e output packet header 
+        totalPacketNumBytes = -1
+        numDetObj           = -1
+        numTlv              = -1
+        subFrameNumber      = -1
+        platform            = -1
+        frameNumber         = -1
+        timeCpuCycles       = -1
+    else: # find the magic number i.e output packet header 
+        totalPacketNumBytes = getUint32(data[headerStartIndex+12:headerStartIndex+16:1])
+        platform            = getHex(data[headerStartIndex+16:headerStartIndex+20:1])
+        frameNumber         = getUint32(data[headerStartIndex+20:headerStartIndex+24:1])
+        timeCpuCycles       = getUint32(data[headerStartIndex+24:headerStartIndex+28:1])
+        numDetObj           = getUint32(data[headerStartIndex+28:headerStartIndex+32:1])
+        numTlv              = getUint32(data[headerStartIndex+32:headerStartIndex+36:1])
+        subFrameNumber      = getUint32(data[headerStartIndex+36:headerStartIndex+40:1])
+        
+    if(False):
+        print("headerStartIndex    = %d" % (headerStartIndex))
+        print("totalPacketNumBytes = %d" % (totalPacketNumBytes))
+        print("platform            = %s" % (platform)) 
+        print("frameNumber         = %d" % (frameNumber)) 
+        print("timeCpuCycles       = %d" % (timeCpuCycles))   
+        print("numDetObj           = %d" % (numDetObj)) 
+        print("numTlv              = %d" % (numTlv))
+        print("subFrameNumber      = %d" % (subFrameNumber))   
+                            
+    return (headerStartIndex, totalPacketNumBytes, numDetObj, numTlv, subFrameNumber)
+
+
+
+
+
+def parser_one_mmw_demo_output_packet(data, readNumBytes, debug=False):
+    """!
+       Parses a single mmWave demo output packet, handling TLVs for detected points, GTRACK targets, and GTRACK point clouds.
+
+        @param data: 1-dimensional byte array from mmWave radar
+        @param readNumBytes: Total number of bytes in the data array
+        @param debug: Boolean flag for enabling debug prints
+
+        @return result: Pass or fail status
+        @return headerStartIndex: Header start index in the packet
+        @return totalPacketNumBytes: Total packet length in bytes
+        @return numDetObj: Number of detected objects
+        @return numTlv: Number of TLVs in the packet
+        @return subFrameNumber: Sub-frame index of the packet
+        @return detected_targets: List of GTRACK targets parsed
+        @return point_cloud: List of point cloud data parsed
+    """
+    headerNumBytes = 44
+    PI = 3.14159265
+
+    detected_targets = []
+    point_cloud = []
+
+    result = TC_PASS
+
+    # Call parser_helper() to locate packet header and retrieve metadata
+    (headerStartIndex, totalPacketNumBytes, numDetObj, numTlv, subFrameNumber) = parser_helper(data, readNumBytes, debug)
+
+    if headerStartIndex == -1:
+        result = TC_FAIL
+        print("************ Frame Fail: Cannot find the magic words ************")
+        return result, -1, -1, -1, -1, -1, detected_targets, point_cloud
+
+    nextHeaderStartIndex = headerStartIndex + totalPacketNumBytes
+
+    # Check for incomplete packets
+    if headerStartIndex + totalPacketNumBytes > readNumBytes:
+        result = TC_FAIL
+        print("********** Frame Fail: Insufficient bytes in buffer ***********")
+        return result, headerStartIndex, totalPacketNumBytes, numDetObj, numTlv, subFrameNumber, detected_targets, point_cloud
+
+    # Start processing TLVs
+    tlvStart = headerStartIndex + headerNumBytes
+    for tlvIndex in range(numTlv):
+        tlvType = getUint32(data[tlvStart:tlvStart + 4])
+        tlvLen = getUint32(data[tlvStart + 4:tlvStart + 8])
+        tlvPayload = data[tlvStart + 8:tlvStart + 8 + tlvLen]
+
+        if False:
+            print(f"TLV {tlvIndex + 1}: Type={tlvType}, Length={tlvLen}")
+
+        # Handle detected points
+        if tlvType == 1:  # MMWDEMO_UART_MSG_DETECTED_POINTS
+            ...
+            #print("")
+
+        # Handle GTRACK Targets
+        elif tlvType == 10:  # GTRACK Targets (Type 10)
+            targets = parse_gtrack_targets(tlvPayload)
+            detected_targets.extend(targets)
+            if debug:
+                for target in targets:
+                    print(f"Target {target['tid']}: pos=({target['posX']:.2f}, {target['posY']:.2f}, {target['posZ']:.2f}), "
+                          f"vel=({target['velX']:.2f}, {target['velY']:.2f}, {target['velZ']:.2f}), "
+                          f"acc=({target['accX']:.2f}, {target['accY']:.2f}, {target['accZ']:.2f})")
+
+  
+        else:
+            ...
+            #print(f"Unhandled TLV Type: {tlvType}")
+
+        # Move to the next TLV
+        tlvStart += 8 + tlvLen
+
+    return result, headerStartIndex, totalPacketNumBytes, numDetObj, numTlv, subFrameNumber, detected_targets, point_cloud
 
 
 
 
 #-------------------Serial port init-------------------------
-def init_serial_port(port, baudrate=BAUDRATE):
+def init_serial_port(port, baudrate):
     """Initializes a serial port for communication."""
     try:
         ser = serial.Serial(port, baudrate, timeout=1)
@@ -65,6 +238,39 @@ def init_serial_port(port, baudrate=BAUDRATE):
 #TODO: implement packet parsing logic
 
 
+def getUint32(data):
+    """!
+       This function coverts 4 bytes to a 32-bit unsigned integer.
+
+        @param data : 1-demension byte array  
+        @return     : 32-bit unsigned integer
+    """ 
+    return (data[0] +
+            data[1]*256 +
+            data[2]*65536 +
+            data[3]*16777216)
+
+def getUint16(data):
+    """!
+       This function coverts 2 bytes to a 16-bit unsigned integer.
+
+        @param data : 1-demension byte array
+        @return     : 16-bit unsigned integer
+    """ 
+    return (data[0] +
+            data[1]*256)
+
+def getHex(data):
+    """!
+       This function coverts 4 bytes to a 32-bit unsigned integer in hex.
+
+        @param data : 1-demension byte array
+        @return     : 32-bit unsigned integer in hex
+    """         
+    #return (binascii.hexlify(data[::-1]))
+    word = [1, 2**8, 2**16, 2**24]
+    return np.matmul(data,word)
+
 
 
 
@@ -77,8 +283,8 @@ if __name__ == "__main__":
         print("No valid configuration commands found. Exiting...")
         exit()
 
-    cfg_port = init_serial_port(COM_PORTS["cfg"])
-    data_port = init_serial_port(COM_PORTS["data"])
+    cfg_port = init_serial_port(COM_PORTS["cfg"],115200)
+    data_port = init_serial_port(COM_PORTS["data"],921600)
 
     if not cfg_port or not data_port:
         print("Error initializing serial ports. Exiting...")
@@ -95,7 +301,9 @@ if __name__ == "__main__":
             if data_port.in_waiting > 0:
                 data = data_port.read(data_port.in_waiting)
                 data_array = np.frombuffer(data, dtype=np.uint8)
-                print(data_array)
+                readNumBytes = len(data_array)
+                parser_one_mmw_demo_output_packet(data_array,readNumBytes,True)
+                time.sleep(0.1)
             
     except KeyboardInterrupt:
         print("\nTerminating...")
